0cedfcc9e3efe7b1588ca31bee48c904d2dcc8bf
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-04-21 17:53:43 -0600

    app 8 addede

--- src/Home.js
+++ src/Home.js
@@ -121,13 +121,14 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="project8" style={{ cursor: "pointer", backgroundSize: "cover", backgroundRepeat: "no-repeat", height: "vh" }}>
-                    <div className='middle'>
-                        <a href="/">
+                    <div className='middle1'>
+                        <a href="http://app-8-of-30.surge.sh/">
                             <div className='projectInfo' style={{ marginTop: "-1.5em" }}>
-                                <h1 className="nav" style={{ color: "#fcbc05", padding: "2%" }}>8. Coming Soon...</h1>
+                                <h1 className="nav" style={{ color: "#fcbc05", padding: "2%" }}>8. Best Advice</h1>
                                 <h4>
+                                Simple advice app to help users with decisions making.
                                 </h4>
-                                <p style={{ color: "lime", padding: "5%", margin: 0 }}></p>
+                                <p style={{ color: "lime", padding: "5%", margin: 0 }}>JavaScript, React, Axios, Advice Slip API, HTML, & CSS.</p>
                             </div>
                         </a>
                     </div>
